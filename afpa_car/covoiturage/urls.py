9923991ab2a7731fa775f029bc6724e4a98ab5eb
SimonB <simon30400@gmail.com>
2018-07-09 17:49:34 +0200

    début du front basique et qui marche pas

--- afpa_car/covoiturage/urls.py
+++ afpa_car/covoiturage/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
-from django.conf.urls import url
+from django.views.generic import TemplateView
 
-from covoiturage import views
 
 urlpatterns = [
     
-    path('', views.accueil),
-
+    path('', TemplateView.as_view(template_name="covoiturage/accueil.html")),
 
 ]
