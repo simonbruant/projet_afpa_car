66fb8c7dc324122ba6572fe15d139ab3e0e0fae8
Thibaud <thibaud.peroua@gmail.com>
2018-07-24 09:45:37 +0200

    changements form updateview

--- afpa_car/covoiturage/forms.py
+++ afpa_car/covoiturage/forms.py
@@ -2,9 +2,9 @@ from django import forms
 
 from users.models import PrivateData
 
-# class PrivateDataUpdateForm(forms.ModelForm):
-#     class Meta:
-#         model = PrivateData
-#         fields = ('phone_number', 'afpa_number')
+class PrivateDataUpdateForm(forms.ModelForm):
+    class Meta:
+        model = PrivateData
+        fields = ('phone_number', 'afpa_number')
 
     
--- afpa_car/covoiturage/views.py
+++ afpa_car/covoiturage/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView, UpdateView
 
-# from .forms import PrivateDataUpdateForm
+from .forms import PrivateDataUpdateForm
 from users.models import PrivateData
 
 
@@ -13,12 +13,14 @@ class DashboardView(LoginRequiredMixin, TemplateView):
 
 
 class PrivateDataUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
-    model = PrivateData
-    fields = ('phone_number', 'afpa_number')
+    # model = PrivateData
+    # fields = ('phone_number', 'afpa_number')
     template_name = 'covoiturage/profil/infos_privees.html'
     success_url = reverse_lazy('covoiturage:infos_privees')
     success_message = "Informations mises à jour"
+    form_class = PrivateDataUpdateForm
 
     def get_object(self, queryset=None):
         user = PrivateData.objects.get(user=self.request.user)
         return user
+        # return self.request.user #pour user
