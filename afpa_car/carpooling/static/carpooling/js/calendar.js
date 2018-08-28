775f26bb694b32307288ad97f7f93ed9475e08f7
SimonB <simon30400@gmail.com>
2018-08-28 12:16:03 +0200

    modif calendrier VueJs

--- afpa_car/carpooling/static/carpooling/js/calendar.js
+++ afpa_car/carpooling/static/carpooling/js/calendar.js
@@ -1,4 +1,5 @@
-
+// import Datepicker from '../../../../../../../_Program/npm install/node_modules/vuejs-datepicker'
+// import {en, es} from '../../../../../../../_Program/npm install/node_modules/vuejs-datepicker/dist/locale'
 
 new Vue({
     el: '#default_week',
@@ -21,10 +22,13 @@ new Vue({
     }
 });
 
+
 new Vue({
+    delimiters: ['[[', ']]'],
     el: '#datepicker',
     components: {
-        vuejsDatepicker
+        vuejsDatepicker,
+
     },
     data: { 
         fr: { 
@@ -32,11 +36,28 @@ new Vue({
             months: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'], 
             monthsAbbr: ['Jan', 'Fév', 'Mar', 'Avr', 'Mai', 'Juin', 'Juil', 'Août', 'Sep', 'Oct', 'Nov', 'Déc'], 
             days: ['Dim', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam'],
-            yearSuffix: '' },
-
+            yearSuffix: '' 
+            },
+        message : '',
         },
+    updated: function() {
 
-    });
+        console.log("update")
+
+    },
+    methods: {
+        dateSelected :function(e){
+            console.log (e),
+            this.message = e
+        },
+        customFormatter(date) {
+            this.message =  moment(date).format('D MMMM YYYY');
+            console.log(this.message)
+        }
+    }
+});
+
+
 
     
 
