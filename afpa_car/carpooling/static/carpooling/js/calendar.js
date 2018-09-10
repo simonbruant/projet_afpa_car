0a5ad2bbd0d0e6f00df545f68aa67f5a7a4ef13a
Demky <demky.box@gmail.com>
2018-09-10 13:23:05 +0200

    Script bouton desactivation jour de la semaine type OK

--- afpa_car/carpooling/static/carpooling/js/calendar.js
+++ afpa_car/carpooling/static/carpooling/js/calendar.js
@@ -5,19 +5,38 @@ new Vue({
     el: '#default_week',
     data: {
         update_default_week: false,
-        preview_default_week : true,
-        burger : true,
+        preview_default_week: true,
+        burger: true,
     },
+    created: function () {
+        for (var i = 0; i < 5; i++) {
+            var element = document.getElementsByName('form-' + i + '-deactivate')[0]
+
+            if (element.checked) {
+                var mornDepTime = document.getElementsByName('form-' + i + '-morning_departure_time')[0]
+                var mornArrTime = document.getElementsByName('form-' + i + '-morning_arriving_time')[0]
+                var eveDepTime = document.getElementsByName('form-' + i + '-evening_departure_time')[0]
+                var hasForStar = document.getElementsByName('form-' + i + '-has_for_start')[0]
+
+                mornDepTime.disabled = element.checked
+                mornArrTime.disabled = element.checked
+                eveDepTime.disabled = element.checked
+                hasForStar.disabled = element.checked
+            }
+        }
+    },
+
     methods: {
         show_update: function () {
             this.update_default_week = true,
-            this.preview_default_week = false,
-            this.burger = false
+                this.preview_default_week = false,
+                this.burger = false,
+                console.log("script lancé")
         },
         hide_update: function () {
             this.update_default_week = false,
-            this.preview_default_week = true,
-            this.burger = true
+                this.preview_default_week = true,
+                this.burger = true
         }
     }
 });
@@ -30,17 +49,17 @@ new Vue({
         vuejsDatepicker,
 
     },
-    data: { 
-        fr: { 
+    data: {
+        fr: {
             language: 'Français',
-            months: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'], 
-            monthsAbbr: ['Jan', 'Fév', 'Mar', 'Avr', 'Mai', 'Juin', 'Juil', 'Août', 'Sep', 'Oct', 'Nov', 'Déc'], 
+            months: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'],
+            monthsAbbr: ['Jan', 'Fév', 'Mar', 'Avr', 'Mai', 'Juin', 'Juil', 'Août', 'Sep', 'Oct', 'Nov', 'Déc'],
             days: ['Dim', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam'],
-            yearSuffix: '' 
-            },
-        message : '',
+            yearSuffix: ''
         },
-    updated: function() {
+        message: '',
+    },
+    updated: function () {
 
         console.log("update")
 
@@ -51,7 +70,7 @@ new Vue({
         //     this.message = e
         // },
         customFormatter(date) {
-            this.message =  moment(date).format('D MMMM YYYY');
+            this.message = moment(date).format('D MMMM YYYY');
             console.log(this.message)
         }
     }
@@ -59,11 +78,11 @@ new Vue({
 
 
 
-    
 
 
-    
 
 
-    
+
+
+
 
