d3accbf9e9241885fe6ed9b3b9303ff82667beb6
Demky <demky.box@gmail.com>
2018-09-13 11:25:51 +0200

    ajout background desactivation div @trajet_type

--- afpa_car/carpooling/static/carpooling/js/calendar.js
+++ afpa_car/carpooling/static/carpooling/js/calendar.js
@@ -1,42 +1,24 @@
-// import Datepicker from '../../../../../../../_Program/npm install/node_modules/vuejs-datepicker'
-// import {en, es} from '../../../../../../../_Program/npm install/node_modules/vuejs-datepicker/dist/locale'
-
-
-// Desactivate inputs @Votre semaine type
-var desactivate_fields = function (element_cliqued) {
-    let i = element_cliqued.id.substr(8, 1)
-
-    var mornDepTime = document.getElementsByName('form-' + i + '-morning_departure_time')[0]
-    var mornArrTime = document.getElementsByName('form-' + i + '-morning_arriving_time')[0]
-    var eveDepTime = document.getElementsByName('form-' + i + '-evening_departure_time')[0]
-    var hasForStar = document.getElementsByName('form-' + i + '-has_for_start')[0]
-
-    mornDepTime.disabled = !mornDepTime.disabled
-    mornArrTime.disabled = !mornArrTime.disabled
-    eveDepTime.disabled = !eveDepTime.disabled
-    hasForStar.disabled = !hasForStar.disabled
-}
 // disable inputs @Votre semaine type
 new Vue({
     el: '#default_week',
     data: {
         update_default_week: false,
-        preview_default_week: true
+        preview_default_week: true, 
     },
     created: function () {
+        
         for (var i = 0; i < 5; i++) {
             var element = document.getElementsByName('form-' + i + '-deactivate')[0]
-
             if (element.checked) {
+                var divBgColor = document.getElementById('card-content-' + i)
                 var mornDepTime = document.getElementsByName('form-' + i + '-morning_departure_time')[0]
                 var mornArrTime = document.getElementsByName('form-' + i + '-morning_arriving_time')[0]
                 var eveDepTime = document.getElementsByName('form-' + i + '-evening_departure_time')[0]
                 var hasForStar = document.getElementsByName('form-' + i + '-has_for_start')[0]
 
-                mornDepTime.disabled = element.checked
-                mornArrTime.disabled = element.checked
-                eveDepTime.disabled = element.checked
-                hasForStar.disabled = element.checked
+                mornDepTime.disabled = mornArrTime.disabled = eveDepTime.disabled = hasForStar.disabled =  element.checked
+
+                divBgColor.style.backgroundColor = "#d4d8db"
             }
         }
     },
@@ -45,26 +27,28 @@ new Vue({
         show_update: function () {
             this.update_default_week = true
             this.preview_default_week = false
-            console.log("script lancé")
         },
         hide_update: function () {
             this.update_default_week = false
             this.preview_default_week = true
         },
-        // disable_fields: function (element_cliqued) {
-        //     let i = element_cliqued.id.substr(8, 1)
+        deactivate_fields: function (element_cliqued) {
+            let i = element_cliqued.id.substr(8, 1)
 
-        //     var mornDepTime = document.getElementsByName('form-' + i + '-morning_departure_time')[0]
-        //     var mornDepTime = document.getElementsByName('form-' + i + '-morning_departure_time')[0]
-        //     var mornArrTime = document.getElementsByName('form-' + i + '-morning_arriving_time')[0]
-        //     var eveDepTime = document.getElementsByName('form-' + i + '-evening_departure_time')[0]
-        //     var hasForStar = document.getElementsByName('form-' + i + '-has_for_start')[0]
+            var divBgColor = document.getElementById('card-content-' + i)
+            var mornDepTime = document.getElementsByName('form-' + i + '-morning_departure_time')[0]
+            var mornArrTime = document.getElementsByName('form-' + i + '-morning_arriving_time')[0]
+            var eveDepTime = document.getElementsByName('form-' + i + '-evening_departure_time')[0]
+            var hasForStar = document.getElementsByName('form-' + i + '-has_for_start')[0]
+            
+            mornDepTime.disabled = !mornDepTime.disabled
+            mornArrTime.disabled = !mornArrTime.disabled
+            eveDepTime.disabled = !eveDepTime.disabled
+            hasForStar.disabled = !hasForStar.disabled
+            
+            divBgColor.style.backgroundColor = divBgColor.style.backgroundColor === ""  ? "#d4d8db" : ""
 
-        //     mornDepTime.disabled = !mornDepTime.disabled
-        //     mornArrTime.disabled = !mornArrTime.disabled
-        //     eveDepTime.disabled = !eveDepTime.disabled
-        //     hasForStar.disabled = !hasForStar.disabled
-        // }
+        }
     }
 });
 
@@ -92,10 +76,6 @@ new Vue({
 
     },
     methods: {
-        // dateSelected :function(e){
-        //     console.log (e),
-        //     this.message = e
-        // },
         customFormatter(date) {
             this.message = moment(date).format('D MMMM YYYY');
             console.log(this.message)
